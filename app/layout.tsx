d96ce7a8a3fd062629945da506eb491618818ce6
Yulian Khai <yulik19971997@gmail.com>
2025-03-09 16:51:11 +0200

    Navbar component Start

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar/Navbar";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
+				<Navbar />
 				<main>{children}</main>
 			</body>
 		</html>
