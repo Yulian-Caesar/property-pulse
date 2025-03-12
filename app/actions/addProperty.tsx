e4db2f55c1841583eaa047a4bf0d160586fb3d78
Yulian Khai <yulik19971997@gmail.com>
2025-03-12 16:29:29 +0200

    Cloudinary image Upload

--- app/actions/addProperty.tsx
+++ app/actions/addProperty.tsx
@@ -4,6 +4,7 @@ import Property from "@/models/Property";
 import { getSessionUser } from "@/utils/getSessionUser";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import cloudinary from "@/config/cloudinary"
 
 async function addProperty(formData: FormData) {
 	await connectDB()
@@ -17,10 +18,7 @@ async function addProperty(formData: FormData) {
 
 	// Access all values from amenities and images
 	const amenities = formData.getAll('amenities');
-	const images = formData
-		.getAll('images')
-		.filter(image => (image as File).name !== '')
-		.map(image => (image as File).name);
+	const images = formData.getAll('images').filter(image => (image as File).name !== '');
 
 	const propertyData = {
 		owner: userId,
@@ -47,8 +45,27 @@ async function addProperty(formData: FormData) {
 			email: formData.get('seller_info.email'),
 			phone: formData.get('seller_info.phone'),
 		},
-		images,
 	}
+
+	const imageUrls = [];
+
+	for (const imageFile of images) {
+		const imageBuffer = await imageFile.arrayBuffer();
+		const imageArray = Array.from(new Uint8Array(imageBuffer))
+		const imageData = Buffer.from(imageArray)
+
+		// Convert to base64
+		const imageBase64 = imageData.toString('base64')
+
+		// Make request to cloudinary
+		const result = await cloudinary.uploader.upload(`data:image/png;base64,${imageBase64}`, {
+			folder: 'propertypulse'
+		})
+
+		imageUrls.push(result.secure_url)
+	}
+
+	propertyData.images = imageUrls;
 
 	const newProperty = new Property(propertyData)
 	await newProperty.save();
