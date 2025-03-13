21167d4ac5f1a4174988c11e16159f0c95786ee6
Yulian Khai <yulik19971997@gmail.com>
2025-03-13 20:12:29 +0200

    Display Search Results

--- app/properties/search-results/page.tsx
+++ app/properties/search-results/page.tsx
@@ -1,7 +1,14 @@
+import PropertyCard from '@/components/PropertyCard/PropertyCard'
+import { PropertyCardType } from '@/components/PropertyCard/PropertyCard.type'
+import PropertySearchForm from '@/components/PropertySearchForm/PropertySearchForm'
 import connectDB from '@/config/database'
 import Property from '@/models/Property'
 import { convertToSerializableObject } from '@/utils/convertToObject'
+import { FilterQuery } from 'mongoose'
+import Link from 'next/link'
+import { FaArrowAltCircleLeft } from 'react-icons/fa'
 
+type PropertyQuery = FilterQuery<typeof Property>;
 
 type SearchResultsPageProps = {
 	searchParams: {
@@ -15,7 +22,7 @@ const SearchResultsPage = async ({ searchParams: { location, propertyType } }: S
 
 	const locationPattern = new RegExp(location, 'i')
 
-	let query = {
+	let query: PropertyQuery = {
 		$or: [
 			{ name: locationPattern },
 			{ description: locationPattern },
@@ -34,9 +41,29 @@ const SearchResultsPage = async ({ searchParams: { location, propertyType } }: S
 	const propertiesQueryResults = await Property.find(query).lean()
 	const properties = convertToSerializableObject(propertiesQueryResults)
 
-	console.log(properties)
 	return (
-		<div>SearchResultsPage</div>
+		<>
+			<section className='bg-blue-700 py-4'>
+				<div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+					<PropertySearchForm />
+				</div>
+			</section>
+			<section className='px-4 py-6'>
+				<div className="container-xl lg:container m-auto px-4 py-6">
+					<Link href='/properties' className='flex items-center text-blue-500 hover:underline mb-3'>
+						<FaArrowAltCircleLeft className='mr-2 mb-1' /> Back To Properties
+					</Link>
+					<h1 className='text-2xl mb-4'>Search Results</h1>
+					{properties.length === 0 ? (<p>No search results</p>) : (
+						<div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+							{properties.map((property: PropertyCardType) => (
+								<PropertyCard key={property._id} property={property} />
+							))}
+						</div>
+					)}
+				</div>
+			</section>
+		</>
 	)
 }
 
