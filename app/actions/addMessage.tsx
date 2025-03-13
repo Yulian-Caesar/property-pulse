e4432c15be1c7f0c3f91c74b5115158679f044c5
Yulian Khai <yulik19971997@gmail.com>
2025-03-13 21:13:34 +0200

    Submit Message & useActionState Hook

--- app/actions/addMessage.tsx
+++ app/actions/addMessage.tsx
@@ -3,7 +3,7 @@ import connectDB from "@/config/database";
 import Message from "@/models/Message";
 import { getSessionUser } from "@/utils/getSessionUser";
 
-async function addMessage(formData: FormData) {
+async function addMessage(previousState: any, formData: FormData) {
 	await connectDB()
 	const sessionUser = await getSessionUser()
 
--- components/PropertyContactForm/PropertyContactForm.tsx
+++ components/PropertyContactForm/PropertyContactForm.tsx
@@ -1,12 +1,35 @@
+'use client'
 import { FaPaperPlane } from "react-icons/fa"
 import { PropertyCardType } from "../PropertyCard/PropertyCard.type"
+import { useActionState, useEffect } from "react";
+import { useFormStatus } from "react-dom";
+import { useSession } from "next-auth/react";
+import addMessage from "@/app/actions/addMessage";
+import { toast } from "react-toastify";
 
 
 const PropertyContactForm = ({ property }: { property: PropertyCardType }) => {
-	return (
+	const { data: session } = useSession()
+	// @ts-ignore
+	const [state, formAction] = useActionState(addMessage, {})
+
+	useEffect(() => {
+		if (state.error) toast.error(state.error)
+		if (state.submitted) toast.success('Message sent successfully')
+	}, [state])
+
+	if (state.submitted) {
+		return (
+			<p className="text-green-500 mb-4">Your message has been sent</p>
+		)
+	}
+
+	return session && (
 		<div className="bg-white p-6 rounded-lg shadow-md">
 			<h3 className="text-xl font-bold mb-6">Contact Property Manager</h3>
-			<form>
+			<form action={formAction}>
+				<input type="hidden" id="property" name="property" defaultValue={property._id} />
+				<input type="hidden" id="recipient" name="recipient" defaultValue={property.owner} />
 				<div className="mb-4">
 					<label
 						className="block text-gray-700 text-sm font-bold mb-2"
@@ -57,14 +80,14 @@ const PropertyContactForm = ({ property }: { property: PropertyCardType }) => {
 				<div className="mb-4">
 					<label
 						className="block text-gray-700 text-sm font-bold mb-2"
-						htmlFor="message"
+						htmlFor="body"
 					>
 						Message:
 					</label>
 					<textarea
 						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 h-44 focus:outline-none focus:shadow-outline"
-						id="message"
-						name="message"
+						id="body"
+						name="body"
 						placeholder="Enter your message"
 					></textarea>
 				</div>
