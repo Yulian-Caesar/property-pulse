500d8d60823ac18d79da9024862c1cc94886b9df
Yulian Khai <yulik19971997@gmail.com>
2025-03-13 18:37:30 +0200

    Check Bookmark Status

--- components/BookmarkButon/BookmarkButon.tsx
+++ components/BookmarkButon/BookmarkButon.tsx
@@ -4,11 +4,26 @@ import { PropertyCardType } from "../PropertyCard/PropertyCard.type"
 import { useSession } from "next-auth/react";
 import { toast } from "react-toastify";
 import bookmarkProperty from "@/app/actions/bookmarkProperty";
+import { useEffect, useState } from "react";
+import checkBookmarkStatus from "@/app/actions/checkBookmarkStatus";
 
 
 const BookmarkButon = ({ property }: { property: PropertyCardType }) => {
 	const { data: session } = useSession()
 	const userId = session?.user?.id
+	const [isBookmarked, setIsBookmarked] = useState(false)
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() => {
+		if (!userId) {
+			setLoading(false)
+			return;
+		}
+		checkBookmarkStatus(property._id).then(res => {
+			setIsBookmarked(res.isBookmarked)
+			setLoading(false)
+		})
+	}, [property._id, userId, checkBookmarkStatus])
 
 	const handleClick = async () => {
 		if (!userId) {
@@ -18,16 +33,19 @@ const BookmarkButon = ({ property }: { property: PropertyCardType }) => {
 
 		bookmarkProperty(property._id).then((res) => {
 			//if (res?.error) return toast.error(res?.error)
+			setIsBookmarked(res.isBookmarked)
 			toast.success(res.message)
 		})
 	}
 
+	if (loading) return <h2>Loadign...</h2>
+
 	return (
 		<button
-			className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+			className={`${isBookmarked ? 'bg-red-500 hover:bg-red-600' : 'bg-blue-500 hover:bg-blue-600'} text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center`}
 			onClick={handleClick}
 		>
-			<FaBookmark className="mr-2" /> Bookmark Property
+			<FaBookmark className="mr-2" /> {isBookmarked ? 'Remove Bookmark' : 'Bookmark Property'}
 		</button>
 	)
 }
