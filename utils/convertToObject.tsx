b24e537b30e3ddd53acffd5f2348f0b1de0d173e
Yulian Khai <yulik19971997@gmail.com>
2025-03-14 22:40:26 +0200

    fix

--- utils/convertToObject.tsx
+++ utils/convertToObject.tsx
@@ -1,4 +1,5 @@
-export function convertToSerializableObject(leanDocument: Record<string, any>) {
+// @ts-ignore
+export function convertToSerializableObject(leanDocument) {
 	for (const key of Object.keys(leanDocument)) {
 		if (leanDocument[key].toJSON && leanDocument[key].toString) {
 			leanDocument[key] = leanDocument[key].toString()
