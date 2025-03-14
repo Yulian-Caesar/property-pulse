ea7aca1acbdeb74c7107523f648d8f6308b78adb
Yulian Khai <yulik19971997@gmail.com>
2025-03-14 21:49:53 +0200

    fix bug

--- components/FeaturedPropertyCard/FeaturedPropertyCard.tsx
+++ components/FeaturedPropertyCard/FeaturedPropertyCard.tsx
@@ -29,16 +29,16 @@ const FeaturedPropertyCard = ({ property }: { property: PropertyCardType }) => {
 	return (
 		<div className="bg-white rounded-xl shadow-md relative flex flex-col md:flex-row">
 			<Image
-				src={property.images[0]}
-				alt={property.name}
+				src={images[0]}
+				alt={name}
 				width={0}
 				height={0}
 				sizes="100vw"
 				className="w-full h-auto rounded-t-xl md:rounded-tr-none md:rounded-l-xl md:w-2/5"
 			/>
 			<div className="p-6">
-				<h3 className="text-xl font-bold">{property.name}</h3>
-				<div className="text-gray-600 mb-4">{property.type}</div>
+				<h3 className="text-xl font-bold">{name}</h3>
+				<div className="text-gray-600 mb-4">{type}</div>
 				<h3 className="absolute top-[10px] left-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right">
 					{getRateDisplay()}
 				</h3>
