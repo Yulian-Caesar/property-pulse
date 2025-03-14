adf249808daab4516ba6c6f19782cd947bc95b93
Yulian Khai <yulik19971997@gmail.com>
2025-03-14 18:48:01 +0200

    Mark as read message

--- components/MessageCard/MessageCard.tsx
+++ components/MessageCard/MessageCard.tsx
@@ -1,8 +1,33 @@
+'use client'
+import { useEffect, useState } from "react"
 import { MessageType } from "./MessageCard.type"
+import markMessageAsRead from "@/app/actions/markMessageAsRead"
+import { toast } from "react-toastify"
 
 const MessageCard = ({ message }: { message: MessageType }) => {
+	const [isRead, setIsRead] = useState(message.read)
+	const [isMounted, setIsMounted] = useState(false)
+
+	useEffect(() => {
+		setIsMounted(true)
+	}, [])
+
+
+	const handleReadClick = async () => {
+		const read = await markMessageAsRead(message._id)
+		setIsRead(read)
+		toast.success(`Mark as ${read ? 'read' : 'mew'}`)
+	}
+
+	if (!isMounted) {
+		return null
+	}
+
 	return (
 		<div className="relative bg-white p-4 rounded-md shadow-md border border-gray-200">
+			{!isRead && (
+				<div className="absolute top-2 right-2 bg-yellow-500 text-white px-2 py-1 rounded-md">New</div>
+			)}
 			<h2 className="text-xl mb-4">
 				<span className="font-bold">Property Inquiry:</span>{' '}
 				{message.property.name}
@@ -29,8 +54,11 @@ const MessageCard = ({ message }: { message: MessageType }) => {
 					{new Date(message.createdAt).toLocaleString()}
 				</li>
 			</ul>
-			<button className="mt-4 mr-3 bg-blue-500 text-white py-1 px-3 rounded-md" >
-				Mark As Read
+			<button
+				className="mt-4 mr-3 bg-blue-500 text-white py-1 px-3 rounded-md"
+				onClick={handleReadClick}
+			>
+				{isRead ? "Mark As New" : "Mark As Read"}
 			</button>
 			<button className="mt-4 bg-red-500 text-white py-1 px-3 rounded-md">
 				Delete
